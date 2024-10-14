07de130500eed5b732781b65c7f672bdf99751b1
MEGUMMY1 <cys10902@nate.com>
2024-10-14 18:10:30 +0900

    Init: 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import "./reset.css";
 import "./App.css";
 
-function App() {
+export default function App() {
   return <div className="App"></div>;
 }
-
-export default App;
