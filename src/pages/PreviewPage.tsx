0be4fa9365ed22c4ba57cd101fc02d8958b10e59
MEGUMMY1 <cys10902@nate.com>
2024-10-18 00:44:03 +0900

    Feat: 미리보기 화면 및 양식지우기 구현

--- src/pages/PreviewPage.tsx
+++ src/pages/PreviewPage.tsx
@@ -1,27 +1,192 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/store";
 import styles from "./Page.module.scss";
+import { useEffect, useRef, useState } from "react";
+import top_arrow from "../assets/dropdown_top.png";
+import down_arrow from "../assets/dropdown_down.png";
 
 export default function PreviewPage() {
   const { title, subtitle, questionForms } = useSelector((state: RootState) => state.form);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [answers, setAnswers] = useState<{ [key: string]: string | string[] }>({});
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleInputChange = (formId: number, value: string | string[]) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [formId]: value,
+    }));
+  };
+
+  const handleClear = () => {
+    setAnswers({});
+    setSelectedOption(null);
+
+    questionForms.forEach((form) => {
+      switch (form.questionType) {
+        case "단답형":
+        case "장문형":
+          handleInputChange(form.id, "");
+          break;
+        case "객관식 질문":
+          handleInputChange(form.id, "");
+          break;
+        case "체크박스":
+          handleInputChange(form.id, []);
+          break;
+        default:
+          break;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
       <div className={styles.components_container}>
+        <div className={styles.title_container}>
+          <div className={styles.title_bar} />
+          <div className={styles.title_wrapper}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.subtitle}>{subtitle}</p>
+            <p className={styles.required}>* 표시는 필수 질문</p>
+          </div>
+        </div>
         {questionForms.map((form) => (
-          <div key={form.id} className={styles.questionItem}>
-            <h3>
-              {form.questionType}
-              {form.questionText}
-            </h3>
-            {form.options.map((option) => (
-              <p key={option.id}>{option.text}</p>
-            ))}
-            {form.isRequired && <span className={styles.required}>*</span>}
+          <div key={form.id} className={styles.form_container}>
+            <div className={styles.form_wrapper}>
+              <div className={styles.form_header}>
+                <h2>{form.questionText}</h2>
+                {form.isRequired && <span className={styles.required}>*</span>}
+              </div>
+              {(() => {
+                switch (form.questionType) {
+                  case "단답형":
+                    return (
+                      <input
+                        type="text"
+                        className={styles.shortanswer}
+                        placeholder="내 답변"
+                        value={answers[form.id] || ""}
+                        onChange={(e) => handleInputChange(form.id, e.target.value)}
+                      />
+                    );
+                  case "장문형":
+                    return (
+                      <textarea
+                        className={styles.longanswer}
+                        placeholder="내 답변"
+                        rows={1}
+                        value={answers[form.id] || ""}
+                        onChange={(e) => handleInputChange(form.id, e.target.value)}
+                        onInput={(e) => {
+                          const target = e.target as HTMLTextAreaElement;
+                          target.style.height = "auto";
+                          target.style.height = `${target.scrollHeight}px`;
+                        }}
+                      />
+                    );
+                  case "객관식 질문":
+                    return form.options.map((option) => (
+                      <div key={option.id} className={styles.checkbox_wrapper}>
+                        <input
+                          type="radio"
+                          id={`option-${option.id}`}
+                          name={`question-${form.id}`}
+                          className={styles.checkbox}
+                          checked={answers[form.id] === option.text}
+                          onChange={() => handleInputChange(form.id, option.text)}
+                        />
+                        <label htmlFor={`option-${option.id}`}>{option.text}</label>
+                      </div>
+                    ));
+                  case "체크박스":
+                    return form.options.map((option) => (
+                      <div key={option.id} className={styles.checkbox_wrapper}>
+                        <input
+                          type="checkbox"
+                          id={`option-${option.id}`}
+                          className={styles.checkbox}
+                          checked={
+                            Array.isArray(answers[form.id]) &&
+                            answers[form.id].includes(option.text)
+                          }
+                          onChange={() => {
+                            const currentAnswers: string[] = Array.isArray(answers[form.id])
+                              ? (answers[form.id] as string[])
+                              : [];
+
+                            if (currentAnswers.includes(option.text)) {
+                              handleInputChange(
+                                form.id,
+                                currentAnswers.filter((ans) => ans !== option.text)
+                              );
+                            } else {
+                              handleInputChange(form.id, [...currentAnswers, option.text]);
+                            }
+                          }}
+                        />
+                        <label htmlFor={`option-${option.id}`}>{option.text}</label>
+                      </div>
+                    ));
+                  case "드롭다운":
+                    return (
+                      <div className={styles.dropdown} ref={dropdownRef}>
+                        <div className={styles.dropdown_input} onClick={() => setIsOpen(!isOpen)}>
+                          <span>{selectedOption || "선택"}</span>
+                          <img
+                            src={isOpen ? top_arrow : down_arrow}
+                            className={styles.dropdown_icon}
+                            width={15}
+                            height={15}
+                            alt="arrow icon"
+                          />
+                        </div>
+                        {isOpen && (
+                          <ul className={styles.dropdown_menu}>
+                            {form.options.map((option) => (
+                              <li
+                                key={option.id}
+                                className={styles.dropdown_option}
+                                onClick={() => {
+                                  handleOptionClick(option.text);
+                                  handleInputChange(form.id, option.text);
+                                }}
+                              >
+                                {option.text}
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </div>
+                    );
+                  default:
+                    return null;
+                }
+              })()}
+            </div>
           </div>
         ))}
+        <button onClick={handleClear}>양식지우기</button>
       </div>
     </div>
   );
